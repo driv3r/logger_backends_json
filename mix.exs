ebd9d9340007c1cd81883179df6b068721de0465
Leszek Zalewski <lz@applift.com>
2016-11-16 18:15:33 +0100

    Release: 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Logger.Backends.JSON.Mixfile do
 
   def project do
     [app: :logger_backends_json,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
